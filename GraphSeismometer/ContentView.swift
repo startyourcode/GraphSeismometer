fab4b5a1bf8e86df48e2aba29a8b3f1c47a7661c
imchino <chino618@me.com>
2022-04-07 15:08:31 +0900

    Initial Commit

---
+++ GraphSeismometer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GraphSeismometer
+//
+//  Created by chino on 2022/04/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GraphSeismometer/GraphSeismometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphSeismometerApp.swift
+//  GraphSeismometer
+//
+//  Created by chino on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct GraphSeismometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
